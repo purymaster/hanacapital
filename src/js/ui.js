96cfe9bd88178cb999a3ebd2a59e29da8e8ff258
RING <purymaster@naver.com>
2025-03-12 13:03:24 +0900

    feat: datepicker, monthpicker 기능 추가

--- src/js/ui.js
+++ src/js/ui.js
@@ -254,6 +254,32 @@
     handleToast();
     handleLoading();
     handleModal();
-    $(".datepicker").datepicker();
+    $(".datepicker").datepicker({
+      dateFormat: 'yy.mm.dd',
+      showMonthAfterYear: true,
+      changeYear: true,
+      changeMonth: true,
+      gotoCurrent: true,
+      showButtonPanel: true,
+      currentText: 'Today',
+      closeText: '닫기',
+      prevText: '이전 달',
+      nextText: '다음 달',
+      monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+      monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+      dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+      dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+      dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+      weekHeader: '주',
+      yearSuffix: '년 '
+    });
+    $(".monthpicker").monthpicker({
+      dateFormat: 'yy.mm',
+      changeYear: true,
+    });
   });
 })(jQuery);
+
+$.datepicker._gotoToday = function (id) {
+  $(id).datepicker('setDate', new Date()).datepicker('hide').blur();
+};
